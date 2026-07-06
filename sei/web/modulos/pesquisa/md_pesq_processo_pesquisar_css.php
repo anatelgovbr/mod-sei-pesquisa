b7b26a741c7bcf69fba982fb7dfbb9c37a28bfe3
Emerson Barbosa Gonçalves <emerson_b.goncalves@outlook.com>
2026-07-06 14:29:39 -0300

    Subindo arquivos da versão 4.3.3

---
+++ sei/web/modulos/pesquisa/md_pesq_processo_pesquisar_css.php
@@ -0,0 +1,33 @@
+<style>
+    .row { 
+        margin-top: 10px;
+    }
+
+    .mb-3 {
+        margin-bottom:0px !important; width: 50% !important
+    }
+
+    .data {
+        width:auto !important; display: inline-block !important;
+    }
+
+    .infraImgModulo {
+        vertical-align: middle;
+    }
+
+    #txtDataInicio {
+        width:70%;
+    }
+
+    #txtDataFim {
+        width:70%;
+    }
+
+    .captcha-loader {
+        overflow: hidden
+    }
+    
+    .capload { 
+        position: absolute; width: 140px; height: 45px; top: 10px; background: rgba(255,255,255,.9); z-index: 99; text-align: center; padding-top: 15px; display: none 
+    }
+</style>
